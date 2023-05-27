944d77be0da427ccb323585142698ff8adf03af2
Alexey Subbotin <alesu1543@gmail.com>
2023-05-27 14:42:29 +0300

    fix logic

--- test.R
+++ test.R
@@ -1,6 +1,7 @@
 # Install package
 #install.packages('RMySQL', repos = "http://cran.us.r-project.org")
 #install.packages('servr', repos = "http://cran.us.r-project.org")
+#install.packages('sjmisc', repos = "http://cran.us.r-project.org")
 
 
 myString <- "Loading..."
@@ -9,57 +10,39 @@ print(myString)
 
 # Loading library
 library("RMySQL")
+library("sjmisc")
  
 # Create connection
-mysqlconn = dbConnect(MySQL(), user = 'root', password = '',
-                      dbname = 'test', host = 'localhost')
- 
-# Show tables in database
-# dbListTables(mysqlconn)
-
-# Select all rows from articles table
+mysqlconn = dbConnect(MySQL(), user = 'root', password = '', dbname = 'test', host = 'localhost', port = 3306)
 res = dbSendQuery(mysqlconn, "SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC")
  
 # Fetch first 3 rows in data frame
 df = fetch(res, n = 3)
 print(df)
 
-viewSelect <- function() {
-list_file <- read.table("select.html", 
-                sep="\t", as.is=TRUE, 
-                check.names=FALSE, comment.char="")
+GetHTML <- function() {
+	list_file <- read.table("select.html", sep="\t", as.is=TRUE, check.names=FALSE, comment.char="")
 
 	print(typeof(list_file))
 	
 	for(line_file in list_file){
 	
+		if (str_contains(line_file, c("@tr", "@ver"), logic = "or") == TRUE) {
+			print(line_file)
+		}
+		if(str_contains(line_file, "@tr") != FALSE){
+			print("TR!!!")
+		}
+		if(str_contains(line_file, "@ver") != FALSE){
+			print("VER!!!")
+		}
+		
+		print(str_contains("trtrtr", "tr"))
 		print(line_file)
-		
-		#if (grepl("@tr", line_file, fixed = TRUE) == TRUE) {
-		
-		#}
-  
 	}
 	
-	#print(y1)
-	#v1 <- list_file
     return(list_file)
 }
 
-
-
-
-# always return 'Success:' followed by the requested path
-s = servr::create_server(handler = function(req) {
-
-list(status = 200L, body = paste(unlist(viewSelect()), collapse=''))
-
-#list(status = 200L, body = paste("Success:", req$PATH_INFO))
-})
-s$url
-browseURL(paste0(s$url, "/hello"))
-browseURL(paste0(s$url, "/world"))
-s$stop_server()
-
-
-print("ok")
+# запуск сервера и ожидание подключений ...
+servr::create_server(handler = function(req) {list(status = 200L, body = paste(unlist(GetHTML()), collapse=''))})
