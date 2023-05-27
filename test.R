2f4968d47871256ae9a63a48925243190f701b2d
Alexey Subbotin <alesu1543@gmail.com>
2023-05-27 22:24:49 +0300

    fix html f. db

--- test.R
+++ test.R
@@ -3,7 +3,6 @@
 #install.packages('servr', repos = "http://cran.us.r-project.org")
 #install.packages('sjmisc', repos = "http://cran.us.r-project.org")
 
-
 myString <- "Loading..."
 print(myString)
 
@@ -11,38 +10,64 @@ print(myString)
 # Loading library
 library("RMySQL")
 library("sjmisc")
- 
-# Create connection
+
+# Создание подключения ...
 mysqlconn = dbConnect(MySQL(), user = 'root', password = '', dbname = 'test', host = 'localhost', port = 3306)
-res = dbSendQuery(mysqlconn, "SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC")
- 
-# Fetch first 3 rows in data frame
-df = fetch(res, n = 3)
-print(df)
+
+# формирование строк таблицы ...
+ViewSelect <- function() {
+	res = dbSendQuery(mysqlconn, "SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC")
+	 
+	df <- dbFetch(res)
+	#print(df)
+	s_out <- list()
+	for(sRow in df){
+		s_out = c(s_out, "<tr>")
+		for(sCol in sRow){
+			s_out = c(s_out, paste("<td>", sCol, "</td>"))
+			print(sCol)
+		}
+		s_out = c(s_out, "</tr>")
+	}
+	
+	dbClearResult(res)
+	return(s_out)
+}
+
+# вервия базы данных ...
+ViewVer <- function() {
+	res = dbSendQuery(mysqlconn, "SELECT VERSION() AS ver")
+	s_ver <- dbFetch(res)
+	dbClearResult(res)
+	return(s_ver)
+}
+
 
 GetHTML <- function() {
 	list_file <- read.table("select.html", sep="\t", as.is=TRUE, check.names=FALSE, comment.char="")
 
 	print(typeof(list_file))
+	list_out <- list()
 	
 	for(line_file in list_file){
 	
-		if (str_contains(line_file, c("@tr", "@ver"), logic = "or") == TRUE) {
-			print(line_file)
+		#if ((!str_contains(line_file, "@tr")) && (!str_contains(line_file, "@ver"))) {
+		if ((!grepl(line_file, "@tr")) && (!grepl(line_file, "@ver"))) {
+			list_out = c(list_out, line_file)
 		}
-		if(str_contains(line_file, "@tr") != FALSE){
-			print("TR!!!")
+		if (str_contains(line_file, "@tr")) {
+			list_out = c(list_out, ViewSelect())
+			print("This is TR.")
 		}
-		if(str_contains(line_file, "@ver") != FALSE){
-			print("VER!!!")
+		if (str_contains(line_file, "@ver")) {
+			list_out = c(list_out, ViewVer())
+			print("This is VER.")
 		}
-		
-		print(str_contains("trtrtr", "tr"))
-		print(line_file)
 	}
 	
-    return(list_file)
+    return(list_out)
 }
 
+print("Listening for connections at http://127.0.0.1:4321/ ...")
 # запуск сервера и ожидание подключений ...
 servr::create_server(handler = function(req) {list(status = 200L, body = paste(unlist(GetHTML()), collapse=''))})
